32b5ea67a5ec38440b343f3168487730f98faed2
Joel Kram <joelnskra@gmail.com>
2025-03-03 13:30:06 +0000

    Implement book addition functionality with form display and input handling

--- index.js
+++ index.js
@@ -14,19 +14,42 @@ class Books {
     constructor(){
 
     }
+
   //add book to library
   addBookToLibrary() {
-    return myLibrary.push(book);
+  let title = $("#title").val();
+  let author = $("#author").val();
+  let numberOfPages = $("#numberOfPages").val();
+  let isFinished = $('#isFinished').prop('checked');
+  
+  //create new object
+  let newBook = new Books(title, author, numberOfPages, isFinished);
+  console.log(newBook);
+  myLibrary.push(newBook)
+  console.log(myLibrary);
+  this.displayLibrary(); //render to screen
   }
   //add books to screen
   displayLibrary() {
     //loop through library and display all books
     for (let i = 0; i < myLibrary.length; i++) {
-      $("#library").html(myLibrary);
+      console.log(myLibrary[i]); //log elements to console
+      
     }
   }
 }
 
 
-
+thisLibrary = new Library;
 //debug station
+//grab book button  
+$("#new-book-btn").on('click', () => {
+  $("#new-book-form").css("display", "flex");
+})
+
+//grab submit button
+$("#new-book-form").on("submit", (event) => {
+  event.preventDefault();
+  alert("submit debug")
+  thisLibrary.addBookToLibrary();
+});
