54b8378ad75a0b75b3486c72febb9fa322436bbf
Joel Kram <joelnskra@gmail.com>
2025-03-03 13:51:57 +0000

    Add Bootstrap for styling and enhance library display functionality

--- index.js
+++ index.js
@@ -31,16 +31,31 @@ class Books {
   }
   //add books to screen
   displayLibrary() {
+    //clear previous library
+    $("#library-container").html("");
+
     //loop through library and display all books
     for (let i = 0; i < myLibrary.length; i++) {
+      let book = myLibrary[i]; //saving each object to this variable at index i
       console.log(myLibrary[i]); //log elements to console
-      
+      let card = `
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">${book.title}</h5>
+          <p class="card-text">Author: ${book.author}</p>
+          <p class="card-text">Pages: ${book.numberOfPages}</p>
+          <button class="btn btn-primary">${book.isFinished ? "Finished" : "Not Finished"}</button>
+        </div>
+      </div>
+    `;
+
+    $("#library-container").append(card);
     }
   }
 }
 
 
-thisLibrary = new Library;
+thisLibrary = new Library();
 //debug station
 //grab book button  
 $("#new-book-btn").on('click', () => {
